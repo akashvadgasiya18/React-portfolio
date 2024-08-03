9be8a09675e773ea4924718f57d6794e956dc76a
akashvadgasiya18 <111360873+akashvadgasiya18@users.noreply.github.com>
2024-08-04 01:12:56 +0530

    now_update

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Home.css";
-import img1 from "../../assets/square.png";
+import img1 from "../../assets/my_avtar.png";
 import Socialicon from "./HomeSocial";
 import ScrollDown from "./ScrollDown";
 import TypeWrite from "./TypeWrite";
@@ -12,14 +12,14 @@ const Home = () => {
       <section className="home container" id="home">
         <div className="intro">
           <img src={img1} alt="" className="home_img" />
-          <h1 className="home__name">Akash Vadgasiya</h1>
+          <h1 className="home__name">I am Akash Vadgasiya</h1>
           {/* <span className="home__education">Full-Stack Web Developer</span> */}
           <TypeWrite />
           <Socialicon />
 
-          <a href="#contact" className="btn">
+          {/* <a href="#contact" className="btn">
             Hire Me
-          </a>
+          </a> */}
 
           <ScrollDown />
         </div>
--- frontend/src/component/Home/TypeWrite.js
+++ frontend/src/component/Home/TypeWrite.js
@@ -1,32 +1,41 @@
 import React from "react";
-import Typewriter from "typewriter-effect";
-import "./Type.css"
+// import Typewriter from "typewriter-effect";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import "./Type.css";
 
 const TypeWrite = () => {
+  const [typeEffect] = useTypewriter({
+    words: ["Full-Stack Developer", "Frontend Developer", "Backend Developer"],
+    loop: {},
+    typeSpeed: 100,
+    deleteSpeed: 40,
+  });
   return (
     <>
       <div className="main-container">
         <div className="type-writter-section">
-
-          <Typewriter
+          <span className="text-1">- {typeEffect} -</span>
+          {/* <Typewriter
             options={{
               autoStart: true,
               loop: true,
             }}
             onInit={(typewriter) => {
               typewriter
-                .typeString('<span class="text-1">Full-Stack Developer</span>')
+                .typeString(
+                  '<span class="text-1">Full-Stack Developer </span>'
+                )
                 .pauseFor(1000)
                 .deleteAll()
-                .typeString('<span class="text-2">Frontend Developer</span>')
+                .typeString('<span class="text-2"> Frontend Developer </span>')
                 .pauseFor(2000)
                 .deleteAll()
-                .typeString('<span class="text-3">Backend Developer</span>')
+                .typeString('<span class="text-3"> Backend Developer </span>')
                 .pauseFor(2000)
                 .deleteAll()
                 .start();
             }}
-          />
+          /> */}
         </div>
       </div>
     </>
